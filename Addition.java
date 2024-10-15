3a06122524caf8a964cb0bf676ec1e1d913fe841
Shavez-alam <shavezalam1627@gmail.com>
2024-10-15 22:40:49 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,25 @@
+package testingBaba;
+
+public class Addition {
+	public static void main(String[] args) {
+		
+		int a=4;
+		int b=9;
+		int c=a+b;
+		System.out.println(c);
+		
+		int e=6;
+		int f=2;
+		int d=e-f;
+		System.out.println(d);
+		
+		int j=a*b;
+		System.out.println(j);
+		
+		int z=e/f;
+		System.out.println(z);
+		
+			
+	}
+
+}
